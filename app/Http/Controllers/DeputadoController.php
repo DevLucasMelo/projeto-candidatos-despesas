7ad8be64b67666b837033ade48072aa7c8502fa5
Lucas Faustini de Melo <faustinilucas8@gmail.com>
2025-07-21 21:40:51 -0300

    refactor: Implementado modificações na lógica das ufs e partidos da DeputadoController e adicionado as novas rotas para informações dos deputados e despesas na route web.php

--- app/Http/Controllers/DeputadoController.php
+++ app/Http/Controllers/DeputadoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Deputado;
+use App\Models\Uf;
+use App\Models\Partido;
 use Illuminate\Http\Request;
 
 class DeputadoController extends Controller
@@ -13,23 +15,46 @@ class DeputadoController extends Controller
             ->when($request->nome, fn($q) =>
                 $q->where('dep_nome', 'like', '%' . $request->nome . '%')
             )
-            ->when($request->partido, function ($q) use ($request) {
+            ->when($request->partido, fn($q) =>
                 $q->whereHas('partido', fn($sub) =>
                     $sub->where('par_nome', $request->partido)
-                );
-            })
-            ->when($request->uf, function ($q) use ($request) {
+                )
+            )
+            ->when($request->uf, fn($q) =>
                 $q->whereHas('uf', fn($sub) =>
                     $sub->where('uf_sigla', $request->uf)
-                );
-            })
-            ->when($request->despesa, function ($q) use ($request) {
+                )
+            )
+            ->when($request->despesa, fn($q) =>
                 $q->whereHas('despesas', fn($sub) =>
                     $sub->where('des_tipo_despesa', 'like', '%' . $request->despesa . '%')
-                );
-            })
+                )
+            )
             ->paginate(12);
 
-        return view('deputados.index', compact('deputados'));
+        $ufs = Uf::orderBy('uf_sigla')->get();
+        $partidos = Partido::orderBy('par_nome')->get();
+
+        return view('deputados.index', compact('deputados', 'ufs', 'partidos'));
     }
+
+    public function show($id)
+    {
+        $deputado = Deputado::with(['despesas', 'partido', 'uf'])->findOrFail($id);
+        return view('deputados.show', compact('deputado'));
+    }
+
+    public function info($id)
+    {
+        $deputado = Deputado::with(['partido', 'uf'])->findOrFail($id);
+        return view('deputados.info', compact('deputado'));
+    }
+
+    public function despesas($id)
+    {
+        $deputado = Deputado::with(['despesas'])->findOrFail($id);
+
+        return view('deputados.despesas', compact('deputado'));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/deputados', [DeputadoController::class, 'index'])->name('deputados.index');
+
+Route::get('/deputados/{id}/info', [DeputadoController::class, 'info'])->name('deputados.info');
+
+Route::get('/deputados/{id}/despesas', [DeputadoController::class, 'despesas'])->name('deputados.despesas');
+
+
