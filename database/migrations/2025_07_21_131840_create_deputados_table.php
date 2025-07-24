165e7e0ca44131e00c0ccded55b5a8711c6b875d
Lucas Faustini de Melo <faustinilucas8@gmail.com>
2025-07-24 11:44:34 -0300

    refactor: Modificação na migration de deputado para atender os novos relacionamentos do bd

--- database/migrations/2025_07_21_131840_create_deputados_table.php
+++ database/migrations/2025_07_21_131840_create_deputados_table.php
@@ -16,8 +16,20 @@ return new class extends Migration {
             $table->unsignedBigInteger('dep_uf_id')->nullable();
             $table->unsignedBigInteger('dep_par_id')->nullable();
 
+            $table->date('dep_data_nascimento')->nullable();
+            $table->string('dep_municipio_nascimento')->nullable();
+            $table->string('dep_escolaridade')->nullable();
+
+            $table->unsignedBigInteger('dep_gab_id')->nullable();
+            $table->unsignedBigInteger('dep_sit_id')->nullable();
+            $table->unsignedBigInteger('dep_con_ele_id')->nullable();
+
             $table->foreign('dep_uf_id')->references('uf_id')->on('ufs')->onDelete('set null');
             $table->foreign('dep_par_id')->references('par_id')->on('partidos')->onDelete('set null');
+
+            $table->foreign('dep_gab_id')->references('gab_id')->on('gabinetes')->onDelete('set null');
+            $table->foreign('dep_sit_id')->references('sit_id')->on('situacoes')->onDelete('set null');
+            $table->foreign('dep_con_ele_id')->references('con_ele_id')->on('condicoes_eleitorais')->onDelete('set null');
 
             $table->timestamps();
         });
